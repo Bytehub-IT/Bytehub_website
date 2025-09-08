e930a053aaacdc7e4173c3035a4bd4257a7a3a4d
Bajeejames7 <bajeejames3@outlook.com>
2025-09-08 14:13:56 +0300

    Add debugging and 404.html for SPA support

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+console.log('React app starting...');
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  console.log('Root element found');
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+} else {
+  console.error('Root element not found!');
+}
